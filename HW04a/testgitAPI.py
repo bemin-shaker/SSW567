c6b1fe5384d2e045503ec1d89b1674942a5f8914
bemin.shaker <beminshaker1@outlook.com>
2022-10-12 13:20:39 -0400

    incremented commit number

--- HW04a/testgitAPI.py
+++ HW04a/testgitAPI.py
@@ -5,7 +5,7 @@ from gitAPI import getRepoData
 
 class TestGetRepoData(unittest.TestCase):
     @mock.patch("gitAPI.getRepoData", return_value=[('amazon-price-tracker', 3), ('bemin-shaker', 24), ('bemin-shaker.github.io', 30), ('Capella', 1), ('CollabHub', 9), ('Complexity', 30), ('COVID-19-Voice-Assistant', 5), ('CS546', 30),
-                                                    ('data-structures-algorithms', 11), ('Musicly', 30), ('ReviewMyCourse', 2), ('SSW215', 7), ('SSW315', 13), ('SSW345', 26), ('SSW567', 19), ('StockPriceDataFeed', 3), ('theStuteApp', 2)])
+                                                    ('data-structures-algorithms', 11), ('Musicly', 30), ('ReviewMyCourse', 2), ('SSW215', 7), ('SSW315', 13), ('SSW345', 26), ('SSW567', 20), ('StockPriceDataFeed', 3), ('theStuteApp', 2)])
     # test correct response
     def testExistingUser(self, mockGetRepoData):
         resp = [('amazon-price-tracker', 3), ('bemin-shaker', 24), ('bemin-shaker.github.io', 30), ('Capella', 1), ('CollabHub', 9), ('Complexity', 30), ('COVID-19-Voice-Assistant', 5), ('CS546', 30),
